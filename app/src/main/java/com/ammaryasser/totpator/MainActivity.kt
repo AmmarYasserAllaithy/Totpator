eb7734f13125c0f0c82a8bcfa3b6937b71747301
Ammar Yasser Allaithy <ammaryasserallaithy@gmail.com>
2024-06-19 11:53:51 +0300

    update main activity

--- app/src/main/java/com/ammaryasser/totpator/MainActivity.kt
+++ app/src/main/java/com/ammaryasser/totpator/MainActivity.kt
@@ -4,44 +4,60 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.compose.rememberNavController
+import com.ammaryasser.totpator.repository.PreferenceRepository
 import com.ammaryasser.totpator.ui.theme.TotpatorTheme
+import kotlinx.coroutines.launch
+
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContent {
-            TotpatorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            val preferenceRepo = PreferenceRepository(LocalContext.current)
+            val isDark = isSystemInDarkTheme()
+            var applyDarkTheme by remember { mutableStateOf(isDark) }
+            var fontId by remember { mutableIntStateOf(0) }
+
+            LaunchedEffect(Unit) {
+                lifecycleScope.launch {
+                    applyDarkTheme = preferenceRepo.getIsDarkTheme(applyDarkTheme)
+                    fontId = preferenceRepo.getFontId(fontId)
                 }
             }
+
+            TotpatorTheme(applyDarkTheme, fontId) {
+                NavGraph(
+                    navController = rememberNavController(),
+                    isDarkTheme = applyDarkTheme,
+                    fontId = fontId,
+                    onToggleTheme = {
+                        applyDarkTheme = !applyDarkTheme
+                        lifecycleScope.launch {
+                            preferenceRepo.putIsDarkTheme(applyDarkTheme)
+                        }
+                    },
+                    onToggleFontFamily = {
+                        fontId = if (fontId == 0) 1 else 0
+                        lifecycleScope.launch {
+                            preferenceRepo.putFontId(fontId)
+                        }
+                    }
+                )
+            }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TotpatorTheme {
-        Greeting("Android")
     }
 }
