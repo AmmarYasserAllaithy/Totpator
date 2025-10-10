94971dec44652e752c03e5a78b2798708a10c1bc
Ammar Yasser <ammaryasser.allaithy+gh@gmail.com>
2025-10-10 12:02:29 +0300

    ui enhance: CounterDown

--- app/src/main/java/com/ammaryasser/totpator/presentation/ui/component/CounterDown.kt
+++ app/src/main/java/com/ammaryasser/totpator/presentation/ui/component/CounterDown.kt
@@ -11,9 +11,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.unit.dp
 import com.ammaryasser.totpator.util.scheduleAtFixedRate
-import java.util.Date
+import java.util.Calendar
 
 
 @Composable
@@ -22,7 +23,10 @@ fun CounterDown(
     color: Color = colorScheme.onBackground,
     onTimeOver: () -> Unit
 ) {
-    val calcProgress = { (duration - Date().seconds % duration) / duration }
+
+    val calcProgress = {
+        (duration - Calendar.getInstance().get(Calendar.SECOND) % duration) / duration
+    }
     var progress by remember { mutableFloatStateOf(calcProgress()) }
 
     LaunchedEffect(Unit) {
@@ -35,9 +39,11 @@ fun CounterDown(
 
     CircularProgressIndicator(
         progress = { progress },
-        modifier = Modifier.size(size = 20.dp),
+        modifier = Modifier.size(size = 22.dp),
         color = color,
-        strokeWidth = 10.dp,
+        strokeCap = StrokeCap.Butt,
+        strokeWidth = 3.dp,
         trackColor = color.copy(alpha = .2f),
     )
+
 }
